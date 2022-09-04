48971209699989ddf03ec87629f4c6f4009c074f
Fábio <36425864+pkfabio@users.noreply.github.com>
2022-09-04 15:51:35 -0300

    Update user.ps1

--- user.ps1
+++ user.ps1
@@ -10,17 +10,18 @@
 #===========================================================================
 
 # Profile
-    Write-Host "Alterando diretório de profile..."
+    Write-Host "Alterando diretorio de profile..."
     New-Item -ItemType "Directory" -Path "D:\Profile" -Force | Out-Null
     Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileList" -Name "ProfilesDirectory" -Type String -Value "D:\Profile"
     
 # Usuário
-    Write-Host "Informe a senha para o novo usuário : " -NoNewline
+    Write-Host "Informe a senha para o novo usuario : " -NoNewline
     $Password = Read-Host -AsSecureString
-    New-LocalUser "pk" -Password $Password -FullName "Fábio" -Description "Usuário padrão"
+    New-LocalUser "pk" -Password $Password -FullName "Fabio" -Description "Usuario padrao"
     Add-LocalGroupMember -Group "Administradores" -Member "pk"
     
 # Nome e Grupo do computador 
-   Rename-Computer -NewName "Fabio2-pc"
-   Add-Computer -WorkGroupName "LAN"
-   Restart-Computer
+    Write-Host "Alterando nome do computador e grupo de trabalho... "
+    Rename-Computer -NewName "Fabio2-pc"
+    Add-Computer -WorkGroupName "LAN"
+    Restart-Computer
